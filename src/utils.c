7d9084844d8bbfa9a6344be560d244749f1035fa
kim hak jun <kimhj9503@naver.com>
2017-10-09 17:55:01 +0900

    utils, 201420899, 17/10/9

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,35 @@
 #include "utils.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+	char store[8096];
+	char *split;
+	int index=0;
+	char *del="\t\n ";
+
+	*argv=(char**)malloc(9000*sizeof(char*));
+	strcpy(store,command);			
+	(*argv)[index]=(char*)malloc(9000*sizeof(char));
+	split=strtok(store,del);
+
+	if(split==NULL){
+		index=1;
+		strcpy((*argv)[0],"");
+	}else{
+		strcpy((*argv)[index],split);
+	}
+
+	while(split!=NULL){
+		index+=1;
+
+		(*argv)[index]=(char*)malloc(9000*sizeof(char));
+		split=strtok(NULL,del);
+		if(split==NULL) break;
+		strcpy((*argv)[index],split);
+	}
+	*argc=index;
 }
