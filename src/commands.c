948b3433dfe3f3d6bfee12383040bf6589b01c10
kim hak jun <kimhj9503@naver.com>
2017-10-09 17:51:02 +0900

    commands, 201420899, 17/10/9

--- src/commands.c
+++ src/commands.c
@@ -1,30 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-
+#include <unistd.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
   if (!validate_cd_argv(argc, argv))
     return -1;
-
-  // TODO: Fill it!
+	if(chdir(argv[1])!=0)
+		printf("%s can't find.\n",argv[1]);
   return 0;
 }
 
 int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
-
-  // TODO: Fill it!
-
+  char buf[1000]; 
+  getcwd(buf,1000);
+  printf("%s\n",buf);
   return 0;
 }
 
 int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+  if(argc!=2){	
+	return 0;
+  }else{
+	if(strcmp(argv[0],"cd")!=0){
+	return 0;
+	}
+	else return 1;
+}
 }
 
 int validate_pwd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+  if(argc!=1){
+	return 0;
+  }else{ 
+	  if(strcmp(argv[0],"pwd")!=0){
+	return 0;
+	}
+	else return 1;
+       }
 }
+
